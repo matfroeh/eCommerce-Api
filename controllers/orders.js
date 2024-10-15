a16480742529eca3736f09fa46470abfd3f6f702
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-10-15 09:57:25 +0200

    model working for showcase (creating Order, adding Products to existing Order) but missing most functions

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,26 +3,48 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ErrorResponse from "../utils/ErrorResponse.js";
 
 export const getOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.findAll({include: Product});
+  const orders = await Order.findAll({ include: [{model: Product }]});
   res.status(200).json(orders);
 });
 
 export const createOrder = asyncHandler(async (req, res) => {
-    const { userId, productId, quantity } = req.body;
-    const order = await Order.create(req.body);
-    const user = await order.getUser();
-    const orderProduct = await OrderProduct.create({ OrderId: order.id, ProductId: productId, quantity });
+  const { userId, productId, quantity } = req.body;
+  const order = await Order.create(req.body);
+  const user = await order.getUser();
+  const orderProduct = await OrderProduct.create({
+    OrderId: order.id,
+    ProductId: productId,
+    quantity,
+  });
 
-    // const product = await order.getProduct();
-    if (!user) {
-        return res.status(404).json({ message: `User with id ${userId} not found` });
-    };
-    // if (!product) {
-    //     return res.status(404).json({ message: `Product with id ${productId} not found` });
-    // };
-    order.dataValues.user = user;
-    order.dataValues.product = orderProduct.productId;
-    // order.dataValues.quantity = quantity;
-    res.status(201).json(order);
-    }
-);
+  // const product = await order.getProduct();
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: `User with id ${userId} not found` });
+  }
+  // if (!product) {
+  //     return res.status(404).json({ message: `Product with id ${productId} not found` });
+  // };
+  order.dataValues.user = user;
+  order.dataValues.product = orderProduct.productId;
+  // order.dataValues.quantity = quantity;
+  res.status(201).json(order);
+});
+
+export const addToOrder = asyncHandler(async (req, res) => {
+  const { orderId, productId, quantity } = req.body;
+  const order = await Order.findByPk(orderId);
+  const orderProduct = await OrderProduct.create({
+    OrderId: orderId,
+    ProductId: productId,
+    quantity,
+  });
+  if (!order) {
+    return res
+      .status(404)
+      .json({ message: `Order with id ${orderId} not found` });
+  }
+  order.dataValues.product = orderProduct.productId;
+  res.status(201).json(order);
+});
